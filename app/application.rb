a1aba750414f8b99a254b0b716a7cd3326d29ae7
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-12 22:20:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    if Time.now.to_i >= 12
+     now = Time.now
+     if now.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
